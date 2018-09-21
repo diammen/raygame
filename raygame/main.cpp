d151f9a5ec0532d400aac8c489a0f17e9f9e8655
Marcel Dao <marcelnamdao@gmail.com>
2018-09-21 12:41:16 -0700

    raygame

--- raygame/main.cpp
+++ raygame/main.cpp
@@ -18,17 +18,37 @@ int main()
 	//--------------------------------------------------------------------------------------
 	int screenWidth = 800;
 	int screenHeight = 450;
-	int multiplier = 10;
+	int multiplier = 5;
+	int currentFrame = 0;
+	int frameCounter = 0;
+	int frameSpeed = 8;
+
+	int slashFrame = 0;
+	int slashFrameCounter = 0;
 
 	Player player = {0,0};
 	Rectangle box = { 0,0,32,32 };
+	Rectangle slashBox = { 0,0,32,32 };
 
 	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-	Image ball = LoadImage("ballshade.png");
-	ImageCrop(&ball, box);
+	Vector2 position = { 100.0f, 100.0f };
+	Vector2 slashPosition = { 150.0f, 100.0f };
 
-	Texture2D texture = LoadTextureFromImage(ball);
+	//Image ball1 = LoadImage("ballshade.png");
+	//ImageCrop(&ball1, box);
+	//box.x = 32;
+	//Image ball2 = LoadImage("ballshade.png");
+	//ImageCrop(&ball2, box);
+	//box.x = 64;
+	//Image ball3 = LoadImage("ballshade.png");
+	//ImageCrop(&ball3, box);
+	//Texture2D texture1 = LoadTextureFromImage(ball1);
+	//Texture2D texture2 = LoadTextureFromImage(ball2);
+	//Texture2D texture3 = LoadTextureFromImage(ball3);
+	Texture2D ballTexture = LoadTexture("ballshade5.png");
+	Texture2D slashTexture = LoadTexture("slash.png");
+
 	SetTargetFPS(60);
 	//--------------------------------------------------------------------------------------
 
@@ -37,22 +57,51 @@ int main()
 	{
 		// Update
 		//----------------------------------------------------------------------------------
-		// TODO: Update your variables here
-		if (IsKeyDown(KEY_A))
+		frameCounter++;
+
+		// idle animation
+		if (frameCounter >= 60 / frameSpeed)
 		{
-			box.x-= 1 * multiplier;
+			frameCounter = 0;
+			currentFrame++;
+
+			if (currentFrame > 12) currentFrame = 0;
+
+			box.x = (float)currentFrame*(float)ballTexture.width / 17;
 		}
-		if (IsKeyDown(KEY_D))
+		if (IsKeyDown(KEY_A) && position.x > 0)
 		{
-			box.x+= 1 * multiplier;
+			position.x -= 1 * multiplier;
+			box.x = 32 * 14;
 		}
-		if (IsKeyDown(KEY_W))
+		if (IsKeyDown(KEY_D) && position.x < screenWidth - 32)
 		{
-			box.y -= 1 * multiplier;
+			position.x += 1 * multiplier;
+			box.x = 32 * 13;
 		}
-		if (IsKeyDown(KEY_S))
+		if (IsKeyDown(KEY_W) && position.y > 0)
 		{
-			box.y += 1 * multiplier;
+			position.y -= 1 * multiplier;
+			box.x = 32 * 15;
+		}
+		if (IsKeyDown(KEY_S) && position.y < screenHeight - 32)
+		{
+			position.y += 1 * multiplier;
+			box.x = 32 * 16;
+		}
+		if (IsKeyDown(KEY_F))
+		{
+			while (slashFrame < 3)
+			{
+				slashFrameCounter++;
+				if (slashFrameCounter >= 60 / frameSpeed)
+				{
+					slashFrameCounter = 0;
+					slashFrame++;
+
+					slashBox.x = (float)slashFrame*(float)slashTexture.width / 3;
+				}
+			}
 		}
 		//----------------------------------------------------------------------------------
 
@@ -62,8 +111,8 @@ int main()
 
 		ClearBackground(GRAY);
 
-		DrawTexture(texture, box.x, box.y, WHITE);
-		//DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		DrawTextureRec(ballTexture, box, position, WHITE);
+		DrawTextureRec(slashTexture, slashBox, slashPosition, WHITE);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
@@ -71,7 +120,7 @@ int main()
 
 	// De-Initialization
 	//--------------------------------------------------------------------------------------
-	UnloadTexture(texture); // Texture unloading
+	UnloadTexture(ballTexture); // Texture unloading
 
 	CloseWindow();        // Close window and OpenGL context
 	//--------------------------------------------------------------------------------------
